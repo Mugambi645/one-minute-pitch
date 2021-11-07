350f370656a0dffa2c60ae208c354603cb77ddcd
mugambi645 <mugambi645@gmail.com>
2021-11-07 14:03:12 +0300

    installed flask-sqlAlchemy and initialized it to use

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from .config import DevConfig
 from .config import config_options
 
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
@@ -11,6 +13,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     # Initializing Flask Extensions
     bootstrap.init_app(app)
+    db.init_app(app)
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config:
     """
     General configurations parent class
     """
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pato:flower2@localhost/watchlist'
 class ProdConfig(Config):
     """
     Production configuration child class
