fe143889f6b745e3d35d18f4f88a7c0d52107f99
mugambi645 <mugambi645@gmail.com>
2021-11-07 13:05:04 +0300

    installed flask-bootstrap and initialized it

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,7 @@ from flask import render_template
 #main route
 @main.route("/")
 def index():
+    """
+    view root page that returns the index page and its data
+    """
     return render_template("index.html")
