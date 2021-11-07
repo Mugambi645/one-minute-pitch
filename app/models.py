6e718f850c02479f96ad25eee0eb41f9c2196c5b
mugambi645 <mugambi645@gmail.com>
2021-11-07 20:39:15 +0300

    corrected user management settings

--- app/models.py
+++ app/models.py
@@ -1,20 +1,38 @@
 from . import db
+from werkzeug.security import check_password_hash, generate_password_hash
 from . import login_manager
+from flask_login import UserMixin
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    email = db.Column(db.String(255),unique = True,index = True)
+    password_hash = db.Column(db.String(255))
 
-
+    pass_secure  = db.Column(db.String(255))
     def __repr__(self):
         return f'User {self.username}'
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f"User {self.user}"
 class Role(db.Model):
     __tablename__ = 'roles'
 
