ef107d3c90ea8e031ffd631d654f2f77d3125a5e
mugambi645 <mugambi645@gmail.com>
2021-11-07 12:52:01 +0300

    added folder files and initialized application

---
+++ app/config.py
@@ -0,0 +1,29 @@
+from instance.config import SECRET_KEY
+import os
+class Config:
+    """
+    General configurations parent class
+    """
+    pass
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    Args:
+    Config:The parent configuration class with parent config settings
+    """
+    pass
+
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    Args:
+    Config: Parent configuration class 
+    """
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+    }   
