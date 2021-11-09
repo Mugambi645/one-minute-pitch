cf4d851af07009797029da2fc59b0f778a2a24ad
mugambi645 <mugambi645@gmail.com>
2021-11-09 14:29:22 +0300

    config location change

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from . import login_manager
 from flask_login import UserMixin
 from datetime import datetime
 @login_manager.user_loader
-
 def load_user(user_id):
     return User.query.get(int(user_id))
 
